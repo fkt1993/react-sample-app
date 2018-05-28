5142ca168356147c39d715ac0077edfffa32e795
深津裕樹 <fkt.x.day1993@gmail.com>
2018-05-28 20:49:53 +0900

    ignore the click if someone has already won or if a square is filled

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ class Board extends React.Component {
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
