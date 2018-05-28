3c5fdcccda7d9e9140ec4e8069ff70b6301f8bd2
深津裕樹 <fkt.x.day1993@gmail.com>
2018-05-28 20:22:05 +0900

    add click handler to Board

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
@@ -25,8 +25,13 @@ class Board extends React.Component {
       squares: Array(9).fill(null),
     };
   }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
   render() {
     const status = 'Next player: X';
