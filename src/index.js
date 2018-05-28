a8f3f83de9760c4016fed2b4e04f7940e65fdd6c
深津裕樹 <fkt.x.day1993@gmail.com>
2018-05-28 20:42:35 +0900

    declaring a winner

--- src/index.js
+++ src/index.js
@@ -30,7 +30,13 @@ class Board extends React.Component {
     return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       <div>
         <div className="status">{status}</div>
