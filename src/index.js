1710ac70827e9d8932848d7da378024f08f26eff
深津裕樹 <fkt.x.day1993@gmail.com>
2018-05-31 22:52:29 +0900

    resolve warning

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ class Game extends React.Component {
         'Move #' + move :
         'Game start';
       return (
-        <li>
+        <li key={move}>
           <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
         </li>
       );
